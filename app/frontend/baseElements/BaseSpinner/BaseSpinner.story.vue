79f414458972d7201fcf4661df1ceaa9f66b70d4
fanomallot <fano@mtomady.com>
2025-01-25 23:23:15 +0300

    add spinner

---
+++ app/frontend/baseElements/BaseSpinner/BaseSpinner.story.vue
@@ -0,0 +1,44 @@
+<template>
+  <Story
+    title="BaseElements / BaseSpinner"
+    :layout="{type: 'grid', width: '100%'}"
+  >
+    <Variant title="Default state">
+      <BaseSpinner font="slim" />
+      <BaseSpinner color="danger" />
+      <BaseSpinner font="fat" />
+      <BaseSpinner
+        font="fatter"
+        color="success"
+      />
+    </Variant>
+
+    <Variant title="Size small">
+      <BaseSpinner
+        font="slim"
+        type="ringY"
+        color="light"
+      />
+      <BaseSpinner type="ringY" />
+      <BaseSpinner
+        font="fat"
+        type="ringY"
+        color="dark"
+      />
+      <BaseSpinner
+        font="fatter"
+        type="ringY"
+        color="warning"
+      />
+    </Variant>
+  </Story>
+</template>
+
+<script setup>
+import BaseSpinner from './BaseSpinner.vue';
+
+</script>
+<style lang="scss" scoped>
+
+
+</style>
---
+++ app/frontend/baseElements/BaseSpinner/BaseSpinner.vue
@@ -0,0 +1,118 @@
+<template>
+  <div
+    class="spinner"
+    :class="{
+      ['size-' + size]: size,
+      ['color-' + color]: color,
+      ['font-' + font]: font,
+      ['type-' + type]: type,
+    }"
+  />
+</template>
+
+<script setup>
+defineProps({
+  color: {
+    default: 'primary',
+    type: String,
+    validator: value => {
+      return ['primary', 'success', 'danger', 'warning', 'light', 'dark'].includes(value);
+    }
+  },
+  size: {
+    default: 'base',
+    type: String,
+    validator: value => {
+      return ['xs', 'sm', 'base', 'lg', 'xl'].includes(value);
+    }
+  },
+  font: {
+    default: 'base',
+    type: String,
+    validator: value => {
+      return ['slim', 'base', 'fat', 'fatter'].includes(value);
+    }
+  },
+  type: {
+    default: 'base',
+    type: String,
+    validator: value => {
+      return ['base', 'ringY'].includes(value);
+    }
+  },
+});
+
+
+</script>
+<style lang="scss" scoped>
+.spinner {
+  @apply w-12 h-12 rounded-full animate-spin
+  border border-solid shadow-md;
+
+  &.size {
+
+  }
+
+  &.color {
+    &-primary {
+     @apply border-blue-700 border-t-transparent;
+    }
+
+    &-dark {
+      @apply border-gray-800 border-t-transparent;
+    }
+
+    &-success {
+      @apply border-green-600 border-t-transparent;
+    }
+
+    &-danger {
+      @apply border-red-700 border-t-transparent;
+    }
+
+    &-light {
+      @apply border-white hover:border-gray-100 border-t-transparent;
+    }
+
+    &-warning {
+      @apply border-yellow-400 border-t-transparent;
+    }
+  }
+
+  &.font {
+    &-base {
+      @apply border-2;
+    }
+
+    &-fat {
+      @apply border-4;
+    }
+
+    &-fatter {
+      @apply border-8;
+    }
+  }
+
+  &.type {
+    &-ringY {
+      @apply border-0 border-y;
+
+      &.font {
+        &-base {
+          @apply border-y-2;
+        }
+
+        &-fat {
+          @apply border-y-4;
+        }
+
+        &-fatter {
+          @apply border-y-8;
+        }
+      }
+    }
+  }
+
+}
+
+</style>
