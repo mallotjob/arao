9753d208502527a46c5cd8f0148d82e60e64d2d2
fanomallot <aafano19@gmail.com>
2026-03-23 14:49:04 +0300

    setup api

--- app/frontend/admin/api/index.js
+++ app/frontend/admin/api/index.js
@@ -5,6 +5,8 @@ axiosApiInstance.interceptors.response.use(camelizeResponseInterceptor);
 axiosApiInstance.interceptors.request.use(snakifyRequestInterceptor);
 
 export default {
+  getMyStats: () => axiosApiInstance.get('/api/users/stats'),
+  getUser: (id) => axiosApiInstance.get(`/api/users/${id}`),
   getWhoAmI: () => axiosApiInstance.get('/api/users/me'),
   getCompanies: () => axiosApiInstance.get('/api/companies'),
   getProducts: () => axiosApiInstance.get('/api/products'),
