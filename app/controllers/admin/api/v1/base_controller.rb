adb2d8f7d6ae6270a993e7a4f15bc2a242f303d5
fanomallot <aafano19@gmail.com>
2026-04-12 13:47:44 +0300

    add meta into data

--- app/controllers/admin/api/v1/base_controller.rb
+++ app/controllers/admin/api/v1/base_controller.rb
@@ -34,11 +34,33 @@ module Admin
         end
 
         def render_collection(collection, serializer_class, options = {})
-          render json: collection, each_serializer: serializer_class, **options
+          meta = options.delete(:meta)
+          serialized = ActiveModelSerializers::SerializableResource.new(
+            collection,
+            each_serializer: serializer_class,
+            adapter: :attributes
+          ).as_json
+
+          # Use model class name as the key (e.g., 'users', 'companies')
+          model_name = collection.first&.class&.name&.downcase || "data"
+          model_name = model_name.pluralize if collection.respond_to?(:length) && collection.length > 1
+
+          response_data = { model_name.to_sym => serialized }
+          response_data[:meta] = meta if meta
+
+          render json: response_data
         end
 
         def render_resource(resource, serializer_class, options = {})
           render json: resource, serializer: serializer_class, **options
+        end
+
+        def meta_attributes(collection, extra_meta = {})
+          is_paginated = collection.respond_to?(:total_count)
+          base_meta = {
+            total_count: is_paginated ? collection.total_count : collection.count
+          }
+          base_meta.merge(extra_meta)
         end
       end
     end
